b8f50df245b2b439e302d03179c6fbf1f915c5f6
Nathaniel <Downeyn21@gmail.com>
2025-07-14 19:01:08 -0400

    started homepage style and structure

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ function HomePage() {
 
     return ( 
         <>
-            <div>
+            <div className="HomepageHeaderWrapper">
                 {/* will all be variables to change with admin section */}
                 <h1>Header / Welcome</h1>
                 <p>Something creative for trust love and something else cute or something bla bla bla bla</p>
