86f8411bf0406dea227c9e589ac23a9a6696de31
Nathaniel <Downeyn21@gmail.com>
2025-07-28 17:11:35 -0400

    homepage hero image added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,9 +2,13 @@ function HomePage() {
 
     return ( 
         <>
-            <div className="HomepageHeaderWrapper">
+            <div className="HomepageContainer">
                 {/* will all be variables to change with admin section */}
-                <h1>Header / Welcome</h1>
+                <div className="HomepageHeaderWrapper">
+                    <h1>Welcome to Willow Springs</h1>
+                    <div className="gradientOverlay"></div>
+                    <img src="/public/images/aiPuppy.jpg" alt="" />
+                </div>
                 <p>Something creative for trust love and something else cute or something bla bla bla bla</p>
                 <button>See Avaliable Pups</button>
             </div>
