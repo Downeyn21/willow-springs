cb54e3354f8ec0d080d0f99c76b371decd4f7601
Nathaniel <Downeyn21@gmail.com>
2025-07-31 16:17:31 -0400

    changed ima scale

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
                 {/* <div className="NavbarSpacer"></div> */}
                 <nav className="navbarContentWrapper">
                     <a>About</a>
-                    <a>Contact Me</a>
+                    <a>Contact Us</a>
                     <a>Puppies</a>
                     <a>Adoption Process</a>
                     <a>Products</a>
