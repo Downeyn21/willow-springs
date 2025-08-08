2d76dbea3dc1b0c42316fcc972d649e962168c64
Jason Howell <howell.jasong@gmail.com>
2025-08-08 13:46:23 -0500

    JasonsTest

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,48 @@
 function About() {
-    return ( 
-        <>
-        <div className="aboutMainContainer">
-            <div className="aboutHeaderContainer">
-                <h2>About Us</h2>
-            </div>
-            <div className="aboutBodyContainer">
-                <div className="aboutFirstWrapper">
-                    <img src="/public/images/person-n-dog.jpg" alt="" />
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat vero id eos veniam. Natus quisquam sunt culpa officiis consequatur atque temporibus voluptas minima laboriosam, beatae saepe aspernatur sint doloribus tenetur!
-                        <br />
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, nam tenetur? Vel recusandae aspernatur aperiam officiis aliquam illum aut nam nobis, in laudantium tenetur adipisci omnis sapiente molestiae beatae error?
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium veniam minima exercitationem eius nostrum voluptatem reiciendis vitae deleniti saepe amet quaerat modi non est odit sint, ipsa molestiae maxime repellendus.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta rerum quisquam, sequi unde ipsum reprehenderit porro minus nulla vel eligendi, mollitia quibusdam expedita ad quaerat officiis tenetur. Debitis, voluptates sint.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum accusantium ex animi est placeat. Explicabo cupiditate quod autem! Ipsum saepe voluptatum in, animi sequi aliquam dolorem placeat deleniti voluptas illum.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, impedit, voluptas id harum non quidem aspernatur quos sunt, architecto libero magni ea aut molestias dolorem corrupti saepe porro ab iusto!
-                    </p>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="aboutMainContainer">
+        <div className="aboutHeaderContainer">
+          <h2>About Us</h2>
         </div>
-        </>
-    );
+        <div className="aboutBodyContainer">
+          <div className="aboutFirstWrapper">
+            <img src="/public/images/person-n-dog.jpg" alt="" />
+            <p>
+              Lorem JASON ipsum dolor sit amet consectetur adipisicing elit.
+              Placeat vero id eos veniam. Natus quisquam sunt culpa officiis
+              consequatur atque temporibus voluptas minima laboriosam, beatae
+              saepe aspernatur sint doloribus tenetur!
+              <br />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, nam
+              tenetur? Vel recusandae aspernatur aperiam officiis aliquam illum
+              aut nam nobis, in laudantium tenetur adipisci omnis sapiente
+              molestiae beatae error?
+            </p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium veniam minima exercitationem eius nostrum voluptatem
+              reiciendis vitae deleniti saepe amet quaerat modi non est odit
+              sint, ipsa molestiae maxime repellendus. Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Soluta rerum quisquam, sequi
+              unde ipsum reprehenderit porro minus nulla vel eligendi, mollitia
+              quibusdam expedita ad quaerat officiis tenetur. Debitis,
+              voluptates sint. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Voluptatum accusantium ex animi est placeat.
+              Explicabo cupiditate quod autem! Ipsum saepe voluptatum in, animi
+              sequi aliquam dolorem placeat deleniti voluptas illum. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Quas, impedit,
+              voluptas id harum non quidem aspernatur quos sunt, architecto
+              libero magni ea aut molestias dolorem corrupti saepe porro ab
+              iusto!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
