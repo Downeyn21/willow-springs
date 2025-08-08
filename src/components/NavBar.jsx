f3d06535272c993a3b40d797d35e14ac71d60e08
Nathaniel Downey <downeyn21@gmail.com>
2025-08-08 18:01:25 -0400

    nav mobil

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+    const [toggle, setToggle] = useState(true)
+
+    function toggleChange() {
+        setToggle(!toggle)
+    }
 
     return ( 
         <>
@@ -10,11 +16,23 @@ function NavBar() {
                 </h1>
                 {/* <div className="NavbarSpacer"></div> */}
                 <nav className="navbarContentWrapper">
-                    <Link to='/about'>About</Link>
-                    <Link to='/contact'>Contact Us</Link>
-                    <Link to='/puppies'>Puppies</Link>
-                    <Link to='/adoption'>Adoption Process</Link>
-                    <Link to='/product'>Products</Link>
+                    <div className="">
+                        <button onClick={toggleChange}>
+                            ///
+                        </button>
+                        {toggle &&
+                        <div>
+                            <Link>test</Link>
+                        </div>
+                        }
+                    </div>
+                    <div className="mediaToggle">
+                        <Link to='/contact'>Contact Us</Link>
+                        <Link to='/about'>About</Link>
+                        <Link to='/puppies'>Puppies</Link>
+                        <Link to='/adoption'>Adoption Process</Link>
+                        <Link to='/product'>Products</Link>
+                    </div>
                 </nav>
             </div>
         </>
