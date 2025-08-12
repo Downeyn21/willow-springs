0b9d84692208c7f13edd3711aae680f92c05636a
Nathaniel <Downeyn21@gmail.com>
2025-08-12 16:13:08 -0400

    moved form into a jsx file

---
+++ src/components/contact/ContactForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+function ContactForm() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [body, setBody] = useState('')
+
+    function contactHandle() {
+        console.log('form submit');
+        
+    }
+
+    function handleNameChange(e) {
+        setName(e.target.value)
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value)
+    }
+
+    function handleBodyChange(e) {
+        setBody(e.target.value)
+    }
+
+    return (  
+        <>
+            <div className="contactFormContainer">
+                <form onSubmit={contactHandle}>
+                    <label>
+                        Full Name:
+                        <input 
+                        type="text"
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                    </label>
+                    <br />
+                    <label>
+                        Email:
+                        <input 
+                        type="text"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    </label>
+                    <br />
+                    <label>
+                        Message
+                        <input
+                        type="text" 
+                        value={body}
+                        onChange={handleBodyChange}
+                    />
+                        </label>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
+import ContactForm from "../components/contact/ContactForm";
 
 function Contact() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [body, setBody] = useState('')
 
-    function contactHandle() {
-        console.log('form submit');
-        
-    }
-
-    function handleNameChange(e) {
-        setName(e.target.value)
-    }
-
-    function handleEmailChange(e) {
-        setEmail(e.target.value)
-    }
-
-    function handleBodyChange(e) {
-        setBody(e.target.value)
-    }
 
     return (
         <>
@@ -29,36 +10,7 @@ function Contact() {
                     <h3>Contact us</h3>
                 </div>
                 <div className="contactMainContainer">
-                    <div className="contactFormContainer">
-                        <form onSubmit={contactHandle}>
-                            <label>
-                                Full Name:
-                                <input 
-                                type="text"
-                                value={name}
-                                onChange={handleNameChange}
-                            />
-                            </label>
-                            <br />
-                            <label>
-                                Email:
-                                <input 
-                                type="text"
-                                value={email}
-                                onChange={handleEmailChange}
-                            />
-                            </label>
-                            <br />
-                            <label>
-                                Message
-                                <input
-                                type="text" 
-                                value={body}
-                                onChange={handleBodyChange}
-                            />
-                            </label>
-                        </form>
-                    </div>
+                    <ContactForm />
                 </div>
             </div>
         </>
